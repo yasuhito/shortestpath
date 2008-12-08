9d76930b1e2b0b9b49616f6426bfb2337c01bb34
Yasuhito TAKAMIYA <yasuhito@takamiya.(none)>
2008-12-08 14:46:37 +0900

    Job クラスの run メソッドをおおまかに書いた

--- job.rb
+++ job.rb
@@ -1,20 +1,30 @@
+require 'popen3/shell'
+
+
 class Job
   def initialize coord1, coord2
+    @coord1 = coord1
+    @coord2 = coord2
   end
 
   
   def run
+    Popen3::Shell.open do | shell |
+      shell.on_failure do
+        raise %{Command "#{ command }" failed.}
+      end
+
+      shell.exec command
+    end
   end
 
 
-  def cmd
-    'sp.heap'
+  ################################################################################
+  private
+  ################################################################################
+
+
+  def command
+    "sp #{ @coord1 } #{ @coord2 }"
   end
 end
-
-
-### Local variables:
-### mode: Ruby
-### coding: utf-8-unix
-### indent-tabs-mode: nil
-### End:
--- job_spec.rb
+++ job_spec.rb
@@ -1,9 +1,38 @@
 require 'job'
 
 
-describe Job do
-  it 'should know commandline string' do
-    job = Job.new( 123.456, 987.654 )
-    job.cmd.should == 'sp.heap' 
+describe Job, 'when starting a SP job' do
+  before :each do
+    @job = Job.new( 123.456, 987.654 )
+    @dummy_shell = 'SHELL'
+    Popen3::Shell.stubs( :open ).yields( @dummy_shell )
+  end
+
+
+  it 'should run SP program' do
+    @dummy_shell.stubs( :on_failure )
+
+    @dummy_shell.expects( :exec ).with( 'sp 123.456 987.654' ).once
+
+    @job.run
+  end
+
+
+  it 'should NOT raise if the SP job succeeded' do
+    @dummy_shell.stubs( :on_failure )
+    @dummy_shell.stubs( :exec )
+
+    lambda do
+      @job.run
+    end.should_not raise_error
+  end
+
+
+  it 'should raise if the SP job failed' do
+    @dummy_shell.expects( :on_failure ).yields
+
+    lambda do
+      @job.run
+    end.should raise_error( %{Command "sp 123.456 987.654" failed.} )
   end
 end
